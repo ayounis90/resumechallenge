4cd89e6ce1bc9b91a5bb33dc12b6271975a4ecac
Aneil Younis <aneil.younis@gmail.com>
2023-07-15 11:52:47 +0000

    create s3 bucket and upload files to it

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,17 +1,28 @@
-resource "aws_s3_bucket" "resumechallenge" {
-  provider = aws.shared
+locals {
+  resume_files = "../resume"
+}
+
+resource "aws_s3_bucket" "resume_challenge" {
+  provider = aws.dev
   bucket   = var.bucketName
 }
 
-/*resource "aws_s3_bucket_website_configuration" "example-config" {
-  bucket = aws_s3_bucket.resumechallenge.bucket
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset(local.resume_files, "**")
+  provider = aws.shared
+  bucket = aws_s3_bucket.resume_challenge.id
+  key = each.key
+  source = "${local.resume_files}/${each.value}"
+}
+
+resource "aws_s3_bucket_website_configuration" "resume_config" {
+  bucket = aws_s3_bucket.resume_challenge.id
   index_document = {
     suffix = "index.html"
   }
 }
 
-resource "aws_s3_bucket_policy" "example-policy" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_policy" "resume_challenge_policy" {
+  bucket = aws_s3_bucket.resume_challenge.id
   policy = templatefile("s3-policy.json", { bucket = var.bucketName })
 }
-*/
